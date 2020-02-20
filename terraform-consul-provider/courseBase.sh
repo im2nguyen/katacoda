4bb770681cb3c511887b37c17f88c307a705b812
Tu Nguyen <im2nguyen@users.noreply.github.com>
2020-02-19 22:42:15 -0800

    updated terraform consul provider to use git instead of assets

--- terraform-consul-provider/courseBase.sh
+++ terraform-consul-provider/courseBase.sh
@@ -1,5 +1,12 @@
+# Clone docker compose files
+git clone git@github.com:im2nguyen/terraform-consul-playground.git
+cd terraform-consul-playground 
+
 # Run Docker Compose up (daemon)
 docker-compose up -d
+
+# Create main.tf working file
+cd ..
 touch main.tf
 
 echo "Ready!"
