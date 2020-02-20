eb398759b20843d75e88ee415d879d7c2499bf57
Tu Nguyen <im2nguyen@users.noreply.github.com>
2020-02-20 09:48:21 -0800

    install terraform and use hashicorp github link

--- terraform-consul-provider/courseBase.sh
+++ terraform-consul-provider/courseBase.sh
@@ -1,12 +1,15 @@
+# Install Terraform and init config
+cd ~
+curl -O https://releases.hashicorp.com/terraform/0.12.20/terraform_0.12.20_linux_amd64.zip
+unzip terraform_0.12.20_linux_amd64.zip -d /usr/local/bin/
+export HOME=~/tutorial
+cd $HOME
+
 # Clone docker compose files
-git clone https://github.com/im2nguyen/terraform-consul-playground.git
-cd terraform-consul-playground 
+git clone https://github.com/hashicorp/getting-started-terraform-consul-provider .
+cd consul-playground 
 
 # Run Docker Compose up (daemon)
 docker-compose up -d
-
-# Create main.tf working file
-cd ..
-touch main.tf
 
 echo "Ready!"
