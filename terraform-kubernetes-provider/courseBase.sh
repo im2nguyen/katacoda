22e916e26480dd3b6a21f13da8d3e2a061aaa8d3
Tu Nguyen <im2nguyen@users.noreply.github.com>
2020-03-02 23:49:26 -0800

    removed tfc config

--- terraform-kubernetes-provider/courseBase.sh
+++ terraform-kubernetes-provider/courseBase.sh
@@ -11,22 +11,5 @@ cd ~
 curl -O https://releases.hashicorp.com/terraform/0.12.20/terraform_0.12.20_linux_amd64.zip
 unzip terraform_0.12.20_linux_amd64.zip -d /usr/local/bin/
 
-# Add Terraform Cloud config
-echo -e "// terraform {
-//   backend \"remote\" {
-//     organization = \"my-organization\"
-//     workspaces {
-//       name = \"learn-terraform-k8s\"
-//     }
-//   }
-// }
-\n\n" > ~/main.tf
-
-cat <<EOT >> ~/.terraformrc
-credentials "app.terraform.io" {
-  token = ""
-}
-EOT
-
 clear
 echo "Ready!"
