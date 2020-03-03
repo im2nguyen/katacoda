e1d023230f663f93ae40bce17cc700c6ac7b5d9f
im2nguyen <im2nguyen@users.noreply.github.com>
2020-03-03 01:22:00 -0800

    copy kube + minikube configuration to ~ so it's more similar to normal working directory

--- terraform-kubernetes-provider/courseBase.sh
+++ terraform-kubernetes-provider/courseBase.sh
@@ -11,6 +11,15 @@ cd ~
 curl -O https://releases.hashicorp.com/terraform/0.12.20/terraform_0.12.20_linux_amd64.zip
 unzip terraform_0.12.20_linux_amd64.zip -d /usr/local/bin/
 
+mkdir home
+
+# copy kube + minikube configuration to ~ so it's more similar to normal working directory
+
+cp ~/.kube/config home/.kube/config
+cp -r ~/.minikube/. ~/home/.minikube
+export HOME=~/home
+cd ~
+
 touch main.tf
 
 clear
