773a77781de62e24c1096eeb790e6fbadf5d9049
Tu Nguyen <im2nguyen@users.noreply.github.com>
2020-03-13 09:36:35 -0700

    switch ubuntu -> bash

--- terraform-consul-provider/courseBase.sh
+++ terraform-consul-provider/courseBase.sh
@@ -4,11 +4,10 @@ apt-get install docker-ce docker-ce-cli containerd.io
 
 # Install unzip - Katacoda docker image doesn't have unzip
 apt-get install unzip
-cd ~
+cd ..
 curl -O https://releases.hashicorp.com/terraform/0.12.20/terraform_0.12.20_linux_amd64.zip
 unzip terraform_0.12.20_linux_amd64.zip -d /usr/local/bin/
-export HOME=~/tutorial
-cd $HOME
+cd ~
 
 # Clone docker compose files
 git clone https://github.com/hashicorp/getting-started-terraform-consul-provider .
