452077c006c3e3f4f42769ca2db906ace791f1f2
Tu Nguyen <im2nguyen@users.noreply.github.com>
2020-08-07 11:03:47 -0700

    hashicups scenario

---
+++ terraform-provider-intro/courseBase.sh
@@ -0,0 +1,25 @@
+# Install Terraform and init config
+# Install docker, unzip - Ubuntu doesn't have docker, pipe to null so future commands work
+apt-get install -y docker-ce docker-ce-cli containerd.io unzip < "/dev/null"
+
+# Install unzip - Katacoda docker image doesn't have unzip
+# apt-get install unzip
+cd ..
+curl -O https://releases.hashicorp.com/terraform/0.13.0-rc1/terraform_0.13.0-rc1_linux_amd64.zip
+unzip terraform_0.12.20_linux_amd64.zip -d /usr/local/bin/
+
+# Clone docker compose files
+git clone https://github.com/hashicorp/learn-terraform-hashicups-provider
+mv learn-terraform-hashicups-provider/* ~
+
+cd ~/learn-terraform-hashicups-provider 
+
+# Run Docker Compose up (daemon)
+docker-compose up
+
+# Go back to workspace directory
+cd ..
+
+clear
+
+echo "Ready!"
