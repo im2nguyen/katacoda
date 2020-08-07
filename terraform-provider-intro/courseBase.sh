c0513493ba6229a385645574659a31e0cb0d9656
Tu Nguyen <im2nguyen@users.noreply.github.com>
2020-08-07 11:32:52 -0700

    removed editor

--- terraform-provider-intro/courseBase.sh
+++ terraform-provider-intro/courseBase.sh
@@ -1,23 +1,12 @@
 # Install Terraform and init config
 
 # Install unzip - Katacoda docker image doesn't have unzip
-cd ..
 curl -O https://releases.hashicorp.com/terraform/0.13.0-rc1/terraform_0.13.0-rc1_linux_amd64.zip
-unzip terraform_0.12.20_linux_amd64.zip -d /usr/local/bin/
-cd ~
+unzip terraform_0.13.0-rc1_linux_amd64.zip -d /usr/local/bin/
 
 # Clone docker compose files
 git clone https://github.com/hashicorp/learn-terraform-hashicups-provider
-# mv learn-terraform-hashicups-provider/* ~
-
 cd ~/learn-terraform-hashicups-provider/docker_compose
 
-# Run Docker Compose up (daemon)
+# Run Docker Compose up
 docker-compose up
-
-# Go back to workspace directory
-cd ..
-
-clear
-
-echo "Ready!"
