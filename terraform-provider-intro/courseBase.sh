e8c663544205b26d65bc331e2461920940a4505c
Tu Nguyen <im2nguyen@users.noreply.github.com>
2020-08-07 11:17:08 -0700

    update startup script

--- terraform-provider-intro/courseBase.sh
+++ terraform-provider-intro/courseBase.sh
@@ -1,6 +1,7 @@
 # Install Terraform and init config
 # Install docker, unzip - Ubuntu doesn't have docker, pipe to null so future commands work
-apt-get install -y docker-ce docker-ce-cli containerd.io unzip < "/dev/null"
+# apt-get install -y docker-ce docker-ce-cli containerd.io unzip < "/dev/null"
+apt-get install -y unzip < "/dev/null"
 
 # Install unzip - Katacoda docker image doesn't have unzip
 # apt-get install unzip
@@ -10,9 +11,9 @@ unzip terraform_0.12.20_linux_amd64.zip -d /usr/local/bin/
 
 # Clone docker compose files
 git clone https://github.com/hashicorp/learn-terraform-hashicups-provider
-mv learn-terraform-hashicups-provider/* ~
+# mv learn-terraform-hashicups-provider/* ~
 
-cd ~/learn-terraform-hashicups-provider 
+cd ~/learn-terraform-hashicups-provider/docker_compose
 
 # Run Docker Compose up (daemon)
 docker-compose up
