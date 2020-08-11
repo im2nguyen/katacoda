c3252f62500cf79d0d5b42efbde258b389c9cfc5
Tu Nguyen <im2nguyen@users.noreply.github.com>
2020-08-11 14:32:02 -0700

    update consul file targets

--- terraform-consul-provider/courseBase.sh
+++ terraform-consul-provider/courseBase.sh
@@ -5,7 +5,6 @@ unzip terraform_0.12.20_linux_amd64.zip -d /usr/local/bin/
 
 # Clone docker compose files
 git clone https://github.com/hashicorp/getting-started-terraform-consul-provider
-mv getting-started-terraform-consul-provider/* ~
 
 cd ~/getting-started-terraform-consul-provider/consul-playground 
 
