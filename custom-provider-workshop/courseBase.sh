9ef14d1c57116b146f385381f2c291a491129e8e
Tu Nguyen <im2nguyen@users.noreply.github.com>
2020-09-10 09:51:57 -0700

    Add custom providers workshop, init commit for testing

---
+++ custom-provider-workshop/courseBase.sh
@@ -0,0 +1,15 @@
+# Install Terraform and init config
+
+# Installs Terraform 0.13
+curl -O https://releases.hashicorp.com/terraform/0.13.2/terraform_0.13.2_linux_amd64.zip
+unzip terraform_0.13.2_linux_amd64.zip -d /usr/local/bin/
+
+# Clone docker compose files
+git clone -b hashiconf-2020 https://github.com/hashicorp/terraform-provider-hashicups
+cd ~/learn-terraform-hashicups-provider/docker_compose
+
+# Prevent `yes` command from accidentally being run
+alias yes=""
+
+# Run Docker Compose up
+docker-compose up
